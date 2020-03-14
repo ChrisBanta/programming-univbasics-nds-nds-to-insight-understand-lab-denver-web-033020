3a0f772997e306d594380ee3622358538b827d3c
Chris Banta <chrisbanta1@gmail.com>
2020-03-14 20:43:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,9 +10,6 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  counter = 0
-    while counter < directors_database[0][:movies].length
-        puts directors_database[0][:movies][counter][:title]
-      counter +=1
+  
       end
 end
